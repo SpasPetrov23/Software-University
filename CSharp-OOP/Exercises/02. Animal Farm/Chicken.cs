52e97f596ba9ac224fb8342019f141b03ea20dda
SpasPetrov <spaspetrov23@gmail.com>
2021-01-18 22:15:48 +0200

    Added an Exercise to Encapsulation.

---
+++ CSharp-OOP/Exercises/02. Animal Farm/Chicken.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (value < MinAge || value > MaxAge)
+                {
+                    throw new ArgumentException("Age should be between 0 and 15.");
+                }
+                this.age = value;
+            }
+        }
+
+        public double ProductPerDay
+        {
+            get
+            {
+                return this.CalculateProductPerDay();
+            }
+        }
+
+        private double CalculateProductPerDay()
+        {
+            if (this.Age <= 3)
+            {
+                return 1.5;
+            }
+            else if (this.Age <= 7)
+            {
+                return 2;
+            }
+            else if (this.Age <= 11)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0.75;
+            }
+        }
+    }
+}
